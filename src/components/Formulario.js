877e0785025d25fc2cfc2295e6adffe7c2164d4c
Raul T <jr.torsa@gmail.com>
2020-09-30 16:55:20 -0600

    addind useMoneda

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
+import useMoneda from "../hooks/useMoneda";
 
 const Boton = styled.input`
   margin-top: 20px;
@@ -19,8 +20,18 @@ const Boton = styled.input`
 `;
 
 const Formulario = () => {
+  const MONEDAS = [
+    { codigo: "USD", nombre: "Dolar de Estados Unidos" },
+    { codigo: "MXN", nombre: "Peso Mexicano" },
+    { codigo: "EUR", nombre: "Euro" },
+    { codigo: "GBP", nombre: "Libra Esterlina" },
+  ];
+  // utilizar moneda
+  const [moneda, SelectMonedas] = useMoneda("Elije tu Moneda", "", MONEDAS);
+
   return (
     <form>
+      <SelectMonedas />
       <Boton type="submit" value="Calcular" />
     </form>
   );
